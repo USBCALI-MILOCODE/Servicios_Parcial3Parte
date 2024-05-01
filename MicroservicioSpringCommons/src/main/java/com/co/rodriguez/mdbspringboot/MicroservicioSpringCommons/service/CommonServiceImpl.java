1a7183863d492ebd5885aff153d8c86f82282d37
miloCODE <juamcamirodri@gmail.com>
2024-04-30 23:33:02 -0500

    fixed errors

--- MicroservicioSpringCommons/src/main/java/com/co/rodriguez/mdbspringboot/MicroservicioSpringCommons/service/CommonServiceImpl.java
+++ MicroservicioSpringCommons/src/main/java/com/co/rodriguez/mdbspringboot/MicroservicioSpringCommons/service/CommonServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 public class CommonServiceImpl <E, R extends CrudRepository<E, Long>> implements CommonService<E> {
 
 	@Autowired
